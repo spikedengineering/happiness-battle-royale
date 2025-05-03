e02e9c7572a7bef9542c8a5267125ac8d7bde2f8
dkc13 <104758599+dkc13@users.noreply.github.com>
2025-05-03 17:14:48 +0200

    + startMatch events

--- resources/gamemode/client.lua
+++ resources/gamemode/client.lua
@@ -9,5 +9,20 @@ end)
 
 Events.Subscribe("clientPrintTimeout", function (timeLeft)
     Game.ClearPrints()
-    Game.PrintStringWithLiteralStringNow("STRING", timeLeft, 999, 1)
+    Game.PrintStringWithLiteralStringNow("STRING", timeLeft, 1100, 1)
+end, true)
+
+Events.Subscribe("clientStartMatch", function ()
+    -- Generate random coordinates between two points
+    local x = math.random() * (-446.05900 - -727.34000) + -727.34000
+    local y = math.random() * (-675.44100 - -1073.68000) + -1073.68000
+    local z = 200  -- Z coordinate is constant
+
+    local clientId = Game.GetPlayerId()
+    local serverId = Player.GetServerID(clientId)
+    local playerChar = Game.GetPlayerChar(clientId)
+
+    Game.SetCharCoordinatesNoOffset(playerChar, x, y, z) -- teleport player to happy island
+
+    --Game.TaskFallAndGetUp(playerChar, 3, 100)
 end, true)
